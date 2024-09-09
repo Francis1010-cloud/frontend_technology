25e41e3b19073c6969344c38694dd2b9451858c3
Francis Victor <francis@revolite.io>
2024-09-09 11:41:54 +0530

    Initial Commit in Main Branch

---
+++ Functionality.js
@@ -0,0 +1,6 @@
+
+function OpenMenu(){
+    var MenuBar = document.getElementById("left-nav")
+    console.log("MenuBar",MenuBar);
+    MenuBar.style.display == "block" ? MenuBar.style.display = "none" : MenuBar.style.display = "block";
+}
