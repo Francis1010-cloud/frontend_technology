5010f34e6eab60bd03bf307e3a1f017caa9a365b
Francis Victor <francis@revolite.io>
2024-09-09 15:46:22 +0530

    commit Navbar

--- Functionality.js
+++ Functionality.js
@@ -1,6 +1,19 @@
 
+window.onscroll = function() {scrollFunction()};
+
 function OpenMenu(){
     var MenuBar = document.getElementById("left-nav")
     console.log("MenuBar",MenuBar);
     MenuBar.style.display == "block" ? MenuBar.style.display = "none" : MenuBar.style.display = "block";
+}
+
+
+function scrollFunction() {
+    document.getElementById("navbar").style.position = "fixed";
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    console.log( document.getElementById("navbar"));
+    document.getElementById("navbar").style.top = "10px"; 
+  } else {
+    document.getElementById("navbar").style.top = "0px";
+  }
 }
